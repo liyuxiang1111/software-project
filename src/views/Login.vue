34787e73e5dc61078580814e23d44ddd03405a43
liyuxiang1111 <739559572@qq.com>
2021-12-20 17:00:14 +0800

    liyuxiang

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,87 +1,61 @@
 <template>
   <div class="login-container">
-    <Top></Top>
-    <div class="login-box">
-      <van-form @submit="onSubmit">
-        <van-field
-          class="name"
-          @touchstart.native.stop="show = true"
-          @blur="pshow = false"
-          v-model="username"
-          name="用户名"
-          label="用户名"
-          placeholder="用户名"
-          :rules="[{ required: true, message: '请填写用户名' }]"
-        />
-        <van-field
-          readonly
-          clickable
-          class="passwd"
-          @touchstart.native.stop="pshow = true"
-          v-model="password"
-          type="password"
-          name="密码"
-          label="密码"
-          placeholder="密码"
-          :rules="[{ required: true, message: '请填写密码' }]"
-        />
-        <div style="margin: 16px">
-          <van-button round block type="info" native-type="onSubmit">提交</van-button>
-        </div>
-      </van-form>
-      <van-number-keyboard :show="show" @blur="show = false" @input="onInput" @delete="onDelete" />
-      <van-number-keyboard v-model="password" :show="pshow" :maxlength="10" @blur="pshow = false" />
+    <img src="@/assets/img/bear.jpg" alt="" />
+    <div class="panel">
+      <input class="name-ipt" type="text" name="" id="" placeholder="输入用户名" />
+      <input class="passwd-ipt" type="passwd" name="" id="" placeholder="输入密码" />
     </div>
+    <div class="login">→</div>
   </div>
 </template>
 
 <script>
-import Top from '@/components/Top.vue'
-import Bottom from '@/components/Bottom.vue'
 export default {
-  components: {
-    Top,
-    Bottom,
-  },
   data() {
     return {
-      username: '',
-      password: '',
-      show: false,
-      pshow: false,
+      name: '',
+      passwd: '',
     }
-  },
-  methods: {
-    onSubmit(values) {
-      console.log('submit', values)
-    },
-    onInput(value) {
-      this.username = this.username + value
-      console.log(this.username)
-    },
-    onDelete() {
-      console.log(this.username.length)
-      this.username = this.username.slice(0, this.username.length - 1)
-    },
-    passwdInput(value) {},
-    passwdDelete() {},
   },
 }
 </script>
 
 <style lang="less" scoped>
 .login-container {
-  .login-box {
-    margin-top: 60px;
-    .name {
-      border: 1px solid #1989fa;
+  padding-top: 100px;
+  img {
+    width: 100px;
+    height: 100px;
+  }
+  .panel {
+    margin-top: 20px;
+    .name-ipt {
+      padding: 5px;
+      width: 300px;
+      height: 35px;
+      background-color: #f4f4f4;
       border-radius: 20px;
+      text-align: center;
     }
-    .passwd {
+    .passwd-ipt {
+      padding: 5px;
       margin-top: 10px;
-      border: 1px solid #1989fa;
+      width: 300px;
+      height: 35px;
+      background-color: #f4f4f4;
       border-radius: 20px;
+      text-align: center;
     }
+  }
+  .login {
+    width: 60px;
+    height: 60px;
+    margin: auto;
+    margin-top: 60px;
+    border-radius: 50%;
+    background-color: #1989fa;
+    font-size: 30px;
+    line-height: 60px;
   }
 }
 </style>
--- src/views/User.vue
+++ src/views/User.vue
@@ -1,9 +1,47 @@
 <template>
-  <div class="user-container">123</div>
+  <div class="user-container">
+    <div class="user-head">
+      <img class="fl" src="@/assets/img/head.jpg" alt="" />
+      <span class="fl">用户id：</span>
+    </div>
+    <van-notice-bar left-icon="volume-o" text="欢迎使用外卖狗熊,祝您用餐愉快" />
+    <div class="setting">用户设置</div>
+    <div class="logout">退出登录</div>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style lang="less">
+.user-container {
+  .user-head {
+    padding: 50px 15px 15px 15px;
+    height: 50px;
+    background-color: #1989fa;
+    img {
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+    }
+    span {
+      margin-left: 10px;
+      margin-top: 30px;
+    }
+  }
+  .setting {
+    padding: 5px;
+    height: 50px;
+    line-height: 50px;
+    background-color: #f4f4f4;
+    border-bottom: 1px solid #cacdd1;
+  }
+  .logout {
+    padding: 5px;
+    height: 50px;
+    line-height: 50px;
+    background-color: #f4f4f4;
+  }
+}
+</style>
